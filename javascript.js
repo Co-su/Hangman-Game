2fa468234d0ecd6ed419c9447ffc2b4c99b41e95
Co-Su <thisusernameisnottaken@gmail.com>
2017-11-20 17:56:46 -0800

    hangman now writes to html.  yay?

--- javascript.js
+++ javascript.js
@@ -1,39 +1,48 @@
-  		"<p>Word: " + answerArray + "</p>"
-        //document.querySelector("#game").innerHTML = html;
+var words = ["fool","magician","priestess","empress","emperor","hierophant","lovers","chariot","strength","hermit","fortune","justice","hanged","death","temperance","devil","tower","star","moon","sun","judgement","world"];
 
-			var words = ["fool","magician","priestess","empress","emperor","hierophant","lovers","chariot","strength","hermit","fortune","justice","hanged","death","temperance","devil","tower","star","moon","sun","judgement","world"];
+var word = words[Math.floor(Math.random() * words.length)];
 
-			var word = words[Math.floor(Math.random() * words.length)];
+var guessLimit = 6;
 
-			var guessLimit = 6;
+var answerArray = [];
 
-			var answerArray = [];
-			
-			for (var i = 0; i < word.length; i++) {
-				answerArray[i] = "_";
-			}
+for (var i = 0; i < word.length; i++) {
+	answerArray[i] = "_";
+}
 
-			var remainingLetters = word.length;
+var remainingLetters = word.length;
 
-			while((remainingLetters > 0) && (guessLimit > 0)){
-				var checkGuess = remainingLetters; 
+function hnnghman() {
+    if (guessLimit===0) {document.getElementById("arcana").innerHTML = "YOU LOSE"
+    }
+    var checkGuess = remainingLetters;
 
-				alert(answerArray.join(" "));
+	(answerArray.join(" "));
 
-				var guess = prompt("Press any key to guess, or click Cancel to stop playing.")
+	document.getElementById("arcana").innerHTML = (answerArray.join(" "));
 
-				var guess = guess.toLowerCase();
+	var guess = document.getElementById("guessBox").value;
 
-				if (guess.length !== 1 ) {
-					alert("Please input one letter");
+	var guess = guess.toLowerCase();
 
-				} else {
-					for (var j = 0; j < word.length; j++) {
+	if (guess.length !== 1 ) {
+		document.getElementById("alerts").innerHTML = "Please input one letter";
+
+	} else {
+		for (var j = 0; j < word.length; j++) {
 						
-						if (word[j] === guess) {
-						answerArray[j] = guess;
-						remainingLetters--;}
-					}
-					if (checkGuess === remainingLetters) {guessLimit--;}	
-				}
-			}
+			if (word[j] === guess) {
+				answerArray[j] = guess;
+				remainingLetters--; 
+				document.getElementById("arcana").innerHTML = answerArray;}
+
+		}
+		if (checkGuess === remainingLetters) {guessLimit--;}	
+	}
+	if (guessLimit===0) {document.getElementById("arcana").innerHTML = "YOU LOSE"
+    } else if (remainingLetters===0) {document.getElementById("alerts").innerHTML = "You win!"
+    }	
+}
+
+
+
